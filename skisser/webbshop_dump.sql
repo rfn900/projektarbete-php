3c26482c470fbf739be4ea4c078c53a9f43f190f
rfn900 <rfn900@gmail.com>
2021-05-24 14:27:43 +0200

    Add SQL dump for db

--- skisser/webbshop_dump.sql
+++ skisser/webbshop_dump.sql
@@ -1,21 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 17, 2021 at 03:02 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.4.16
+-- Host: localhost:8889
+-- Generation Time: May 24, 2021 at 12:26 PM
+-- Server version: 5.7.32
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `webbshop`
@@ -31,8 +24,26 @@ CREATE TABLE `orders` (
   `id` int(50) NOT NULL,
   `product_id` int(50) NOT NULL,
   `user_id` int(50) NOT NULL,
-  `shipped` tinyint(1) NOT NULL
+  `shipped` tinyint(1) NOT NULL,
+  `quantity` int(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `product_id`, `user_id`, `shipped`, `quantity`) VALUES
+(1, 2, 4, 1, 2),
+(1, 4, 4, 1, 1),
+(1, 6, 4, 1, 1),
+(2, 2, 4, 1, 1),
+(3, 2, 4, 1, 1),
+(4, 6, 4, 1, 1),
+(5, 2, 4, 0, 1),
+(5, 9, 4, 0, 1),
+(6, 7, 4, 1, 1),
+(7, 2, 6, 0, 1),
+(8, 11, 5, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -42,8 +53,22 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_swedish_ci NOT NULL
+  `name` varchar(255) COLLATE utf8mb4_swedish_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_swedish_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_swedish_ci NOT NULL,
+  `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `image`, `description`, `price`) VALUES
+(4, 'Dog hat', 'https://images-na.ssl-images-amazon.com/images/I/7174hEzc%2BYL._SL1500_.jpg', 'Cute and ugly dog hat at the same time', 239),
+(6, 'Cat protective gear', 'img/cat-gear.png', 'It protects your cat during home renovation', 1990),
+(7, 'Cat hammer and wrench', 'img/cat-hammer.png', 'Well it is a hammer and wrench for your cat', 599),
+(11, 'Cat sport car', 'img/catbil.png', 'Your cat is going to want this', 20999),
+(13, 'Dog socks', 'img/dog-sock.jpeg', 'Dog socks to keep your dog warm', 129);
 
 -- --------------------------------------------------------
 
@@ -62,9 +87,14 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `admin`) VALUES
-(1, 'user1', 0),
+(1, 'user1', 1),
 (2, 'fadfadf', 0),
-(3, 'user2', 0);
+(3, 'user2', 0),
+(4, 'rona2001', 1),
+(5, 'rfn900@gmail.com', 0),
+(6, 'rfn900', 0),
+(7, 'Duck', 0),
+(8, 'Donald', 0);
 
 --
 -- Indexes for dumped tables
@@ -96,22 +126,16 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
@@ -121,10 +145,5 @@ ALTER TABLE `user`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
